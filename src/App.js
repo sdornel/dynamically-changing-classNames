40f7fcfba19289416a0fd0df56cf655d1c6798b8
kaidornel <kai_dornel@hotmail.com>
2020-08-24 12:35:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      clicked: false,
+      activeStars: 0
+    }
+  }
+
+  click = (event) => {
+    this.setState({
+      activeStars: parseInt(event.target.id),
+      clicked: true
+    })
+  }
+
+  render(){
+      return (
+        <div className="stars-div">
+          {this.state.clicked === false ?
+          <div>
+            <span id="1" onClick={(event) => this.click(event)}>*</span>
+            <span id="2" onClick={(event) => this.click(event)}>*</span>
+            <span id="3" onClick={(event) => this.click(event)}>*</span>
+            <span id="4" onClick={(event) => this.click(event)}>*</span>
+            <span id="5" onClick={(event) => this.click(event)}>*</span>
+          </div>
+          :
+          null
+          }
+        </div>
+      // :
+      // null
+      // }
+      // {this.state.clicked === false ? 
+      //   <div>
+
+      //   </div>
+      //   : 
+      //   null}
+    )
+  }
 }
+
+// {this.state.email === "nofacialrec@demo.com" && this.state.password === "demo" ? 
+// <div>
+//     <Form onSubmit={this.handleLoginSubmit}>
+//         <Button type="submit">Login</Button>
+//     </Form>
+// </div>
+// : 
+// <Button type="submit">Move on to facial recognition</Button>
+// }
 
 export default App;
